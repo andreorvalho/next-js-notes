15c84f624dd09369081c7e44c255a366f59bf8ef
andre.orvalho <andre.orvalho@gmail.com>
2025-10-12 11:20:37 +0200

    redirects logged in users

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { signIn } from 'next-auth/react';
+import { signIn, getSession } from 'next-auth/react';
+import type { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
@@ -166,3 +167,20 @@ export default function Login() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
