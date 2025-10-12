eb8abdd1bc8db82ac34360bc0944d70cc1a6d102
andre.orvalho <andre.orvalho@gmail.com>
2025-10-12 09:40:23 +0200

    updated cypress config

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+// Cypress config in CommonJS to avoid Node --loader / tsx on CI
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // Reuse existing plugins file
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+  },
+});
+
+
